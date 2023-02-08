bcac3ebc28672a21464bcd37d0513da93fdde4db
Jikhan Jung <honestjung@gmail.com>
2023-02-08 16:35:39 +0900

    crawler with notion integration

--- scripts/newscrawler.py
+++ scripts/newscrawler.py
@@ -1,9 +1,13 @@
+from decouple import config
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.sciencedaily.com/releases/2023/01/230125121550.htm'
+NOTION_KEY = config("NOTION_KEY")
+print(NOTION_KEY)
+
+url = 'http://34.64.158.160/marinecroc.html'
 page = requests.get(url)
-print(page.content)
+#print(page.content)
 soup = BeautifulSoup(page.content, 'html.parser')
 headline = soup.find(id='headline')
 date_posted = soup.find(id='date_posted')
@@ -12,4 +16,24 @@ first = soup.find(id='first')
 text = soup.find(id='text')
 journal_references = soup.find(id='journal_references')
 
-print(headline)
+if False:
+    print(headline.text)
+    print(date_posted.text)
+    print(first.text)
+    print(abstract.text)
+
+    print(text.text)
+    print(journal_references.text)
+
+import os
+import json
+
+headers = {'Authorization': f"Bearer {NOTION_KEY}", 
+           'Content-Type': 'application/json', 
+           'Notion-Version': '2022-06-28'}
+search_params = {"filter": {"value": "page", "property": "object"}}
+search_response = requests.post(
+    f'https://api.notion.com/v1/search', 
+    json=search_params, headers=headers)
+
+print(search_response.json())           
